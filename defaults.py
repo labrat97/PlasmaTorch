5f60c57156b0abb3abf8c71593a9f945f6d3ef3d
labrat97 <jamesmcroberts82997@gmail.com>
2021-09-17 23:58:50 -0700

    Added/modified more constants

--- defaults.py
+++ defaults.py
@@ -1,5 +1,8 @@
 import torch
 
-DEFAULT_FFT_SAMPLES = 128
+DEFAULT_FFT_SAMPLES = 384
+KNOTS_WITHOUT_LOSS = 8
+# a peak and a trough are needed at minimum for a legible signal
+DEFAULT_KNOT_WAVES = int(DEFAULT_FFT_SAMPLES / (KNOTS_WITHOUT_LOSS * 2))
 DEFAULT_DTYPE = torch.float32
 DEFAULT_SPACE_PRIME = 11
