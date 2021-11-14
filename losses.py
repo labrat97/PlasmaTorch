a853ce0390884d35dcffce976ad3bd6fe925e9d0
labrat97 <jamesmcroberts82997@gmail.com>
2021-11-13 17:37:52 -0800

    Documentation and a functional prototype for bloodmuck

--- losses.py
+++ losses.py
@@ -12,6 +12,16 @@ import torch.fft as tfft
 
 @ts
 def correlation(x:t.Tensor, y:t.Tensor, dim:int=-1) -> t.Tensor:
+    """Find the standard cross-correlation between the natural signals provided.
+
+    Args:
+        x (t.Tensor): One set of signals to use for the final computation.
+        y (t.Tensor): Another set of signals to use for the final compuation.
+        dim (int, optional): The dimension to apply the computation to. Defaults to -1.
+
+    Returns:
+        t.Tensor: The cross-correlation of the two input signals.
+    """
     # Some size assertions/extractions
     xsize = x.size()
     ysize = y.size()
@@ -28,14 +38,25 @@ def correlation(x:t.Tensor, y:t.Tensor, dim:int=-1) -> t.Tensor:
     return corr
 
 
-HYDX_CORRMEAN:int = 0
-HYDX_CORRMIN:int = 1
-HYDX_CORRMAX:int = 2
-HYDX_CORRMEDIAN:int = 3
-HYDX_CORRMODE:int = 4
-HYDX_CORRMSE:int = 5
 @ts
-def hypercorrelation(x:t.Tensor, y:t.Tensor, cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE, dim:int=-1, fullOutput:bool=False, extraTransform:bool=False):
+def hypercorrelation(x:t.Tensor, y:t.Tensor, cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE, \
+    dim:int=-1, fullOutput:bool=False, extraTransform:bool=False) -> t.Tensor:
+    """Run the cross-correlation function accross every single frequency-space domain
+    superposition that can fall out of the input signals. If specified, perform some
+    analysis on the output of the signal, otherwise, give the full square of the domain
+    superposition back to the user.
+
+    Args:
+        x (t.Tensor): One set of signals to use for the final computation.
+        y (t.Tensor): Another set of signals to use for the final computation.
+        cdtype (t.dtype, optional): The complex datatype to use. Defaults to DEFAULT_COMPLEX_DTYPE.
+        dim (int, optional): The dimension to perform the computation on. Defaults to -1.
+        fullOutput (bool, optional): If true, return the raw output of the hypercorrelation function. Defaults to False.
+        extraTransform (bool, optional): If true, run through one more layer of fourier transforms to verify that the signals aren't super lossy in a continuous sense. Defaults to False.
+
+    Returns:
+        t.Tensor: A set of all of the possible correlation functions attached to the end of the input tensor shape.
+    """
     # Some size assertions/extractions
     xsize = x.size()
     ysize = y.size()
@@ -98,8 +119,38 @@ def hypercorrelation(x:t.Tensor, y:t.Tensor, cdtype:t.dtype=DEFAULT_COMPLEX_DTYP
     # Return as a single tensor in the previously commented/written order
     return t.stack((corrmean, corrmin, corrmax, corrmedian, corrmode), dim=-1)
 
+# Some constants for indexing the hypercorrelation function
+HYDX_CORRMEAN:int = 0
+HYDX_CORRMIN:int = 1
+HYDX_CORRMAX:int = 2
+HYDX_CORRMEDIAN:int = 3
+HYDX_CORRMODE:int = 4
+HYDX_CORRMSE:int = 5
+
+
 @ts
-def skeeter(teacher:t.Tensor, student:t.Tensor, center:t.Tensor, teacherTemp:float=1., studentTemp:float=1., dim:int=-1, cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE):
+def skeeter(teacher:t.Tensor, student:t.Tensor, center:t.Tensor, teacherTemp:float=1., \
+    studentTemp:float=1., dim:int=-1, cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE) -> t.Tensor:
+    """A loss used to try to replicate the neat results of the DINO paper's cross-correlation loss.
+    The loss itself is not really mathematically similar at all, however the correlation that gets produced
+    from the function is similar to the correlation that would get produced from the loss function in mention.
+    To do this, the hypercorrelation function is used, then the losses are added together, much the same as parrallel
+    impedance, to produce a final composite loss based around the hypercorrelation mean, square mean, median, and mode.
+
+    Args:
+        teacher (t.Tensor): The teacher, or view dominant tensor output to use for computation. This tensor's
+        gradient is detached immediately.
+        student (t.Tensor): The student, or view dependent tensor output with full gradient attachment.
+        center (t.Tensor): The centering tensor, with gradient attachment, used for modifying the teacher's say
+        on the situation.
+        teacherTemp (float, optional): The temperature of the teacher's evaluation. Defaults to 1.
+        studentTemp (float, optional): The temperature of the student's evaluation. Defaults to 1.
+        dim (int, optional): The dimension to compute the loss only. Defaults to -1.
+        cdtype (t.dtype, optional): The complex datatype to use. Defaults to DEFAULT_COMPLEX_DTYPE.
+
+    Returns:
+        t.Tensor: The parallel impedance style inverse hypercorrelation for each value.
+    """
     # Find the maximum amount of samples on the loss dim
     tenure:t.Tensor = teacher.detach()
     tsize = tenure.size()
@@ -128,4 +179,9 @@ def skeeter(teacher:t.Tensor, student:t.Tensor, center:t.Tensor, teacherTemp:flo
     corrmse = 1. / hypercorr[..., HYDX_CORRMSE]
 
     # Add together the signals in the style of parallel impedance
-    return 1. / (corrmean + corrmedian + corrmode + corrmse)
+    return -1. / (corrmean + corrmedian + corrmode + corrmse)
+
+@ts
+def bloodmuck(teacher:t.Tensor, student:t.Tensor, center:t.Tensor, teacherTemp:float=1., \
+    studentTemp:float=1., dim:int=-1, cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE) -> t.Tensor:
+    return t.zeros((1))
