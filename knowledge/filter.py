8cc7b9e52808a98a997a2a58088c046c71a3c3f2
labrat97 <jamesmcroberts82997@gmail.com>
2021-11-15 11:20:05 -0800

    Removed turbulence from base definition

--- knowledge/filter.py
+++ knowledge/filter.py
@@ -45,7 +45,6 @@ class KnowledgeFilter(nn.Module):
     internal knowledge graph.
     """
     def __init__(self, child:KnowledgeFilterProto, childInputSize:torch.Size, childOutMode:KnowledgeOutputMode=KnowledgeOutputMode.LOGITS, \
-        needsEncoding:bool=True, encoderDimensions:int=DEFAULT_SPACE_PRIME, encoderFullOutput:bool=True, \
         attemptReshape:bool=True, alignCorners:bool=True, outSmearSamples:int=DEFAULT_FFT_SAMPLES, \
         outputKnotCurves:int=None, dtype:torch.dtype=DEFAULT_DTYPE):
         """Builds a knowledge filter with the specified parameters.
@@ -54,9 +53,6 @@ class KnowledgeFilter(nn.Module):
             child (KnowledgeFilterProto): The internal knowledge filter to be used.
             childInputSize (torch.Size): The input size of the contained knowledge graph.
             childOutMode (KnowledgeOutputMode, optional): The mode of output used by the contained knowledge graph. Defaults to KnowledgeOutputMode.LOGITS.
-            needsEncoding (bool, optional): If encoding is needed for the input signal from the provided continuous, sampled, one. Defaults to True.
-            encoderDimensions (int, optional): The amount of curves used by the encoder. Defaults to DEFAULT_SPACE_PRIME.
-            encoderFullOutput (bool, optional): Use the full output of the signal encoder to the knowledge graph and not a collapsed one. Defaults to True.
             attemptReshape (bool, optional): Attempt to reshape the output of the knowledge graph to fit the following functions better. Defaults to True.
             alignCorners (bool, optional): If using the full output, align the corners of the signal during remapping to attempt to force more continuous subsignals. Defaults to True.
             outSmearSamples (int, optional): The amount of samples to be smeared out of the output logits if in logit mode. Defaults to DEFAULT_FFT_SAMPLES.
@@ -64,13 +60,6 @@ class KnowledgeFilter(nn.Module):
             dtype (torch.dtype, optional): The datatype for the internal datastructures and operations. Defaults to DEFAULT_DTYPE.
         """
         super(KnowledgeFilter, self).__init__()
-
-        # Build a turbulence encoder to figure out what's being looked at
-        if needsEncoding:
-            self.encoder:Turbulence = Turbulence(internalWaves=encoderDimensions, \
-                sameDimOut=encoderFullOutput, dtype=dtype)
-        else:
-            self.encoder:Turbulence = None
         
         # Copy child interfacing parameters
         self.inputReshape:bool = attemptReshape
@@ -92,24 +81,18 @@ class KnowledgeFilter(nn.Module):
             self.outKnot:Knot = Knot(knotSize=outputKnotCurves, knotDepth=int(outSmearSamples/outputKnotCurves), dtype=complexType)
 
     def forward(self, x:torch.Tensor, oneD:bool=False, batchDims:int=1) -> torch.Tensor:
-        # Extract subportions of the original signal through embedding
-        if self.encoder is None:
-            xEmbed:torch.Tensor = x
-        else:
-            xEmbed:torch.Tensor = self.encoder.forward(queries=x, states=x, oneD=oneD).flatten(-2, -1)
-
         # Attempt a reshaping of the input signal
         if self.inputReshape:
-            xEmbed = xEmbed.unsqueeze(-2)
-            knowledgeSize = xEmbed.size()[:-2]
+            x = x.unsqueeze(-2)
+            knowledgeSize = x.size()[:-2]
             for n in self.childInputSize:
                 knowledgeSize.append(n)
             
-            knowledgeInputs:torch.Tensor = nnf.interpolate(xEmbed, size=self.flatChild, \
+            knowledgeInputs:torch.Tensor = nnf.interpolate(x, size=self.flatChild, \
                 mode='linear', align_corners=self.alignCorners)
             knowledgeInputs = torch.reshape(knowledgeInputs, shape=knowledgeSize)
         else:
-            knowledgeInputs:torch.Tensor = xEmbed
+            knowledgeInputs:torch.Tensor = x
 
         # Run knowledge through the child
         logits:torch.Tensor = self.child.forward(knowledgeInputs)
