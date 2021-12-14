Fixed the softmax functions after deep pondering

attention.py
@@ -108,9 +108,9 @@ class Turbulence(nn.Module):
 
         
         # Pay attention using spatial warping and basis vector compression
-        warpKnot:torch.Tensor = isoftmax(self.warpKnot.forward(superSum), dim=-2)
-        compressorKnot:torch.Tensor = isoftmax(self.compressorKnot.forward(superSum), dim=-2) * self.compressorGain
-        compressorKnot.squeeze_(dim=1)
+        warpKnot:torch.Tensor = nsoftmax(self.warpKnot.forward(superSum), dim=[-1,-2])
+        compressorKnot:torch.Tensor = nsoftmax(self.compressorKnot.forward(superSum), dim=[-1,-2])
+        compressorKnot.mul_(self.compressorGain)
 
         # Warping as if the state vector is 4D image data as seen here:
         # https://pytorch.org/docs/stable/generated/torch.nn.functional.grid_sample.html#torch.nn.functional.grid_sample

entanglement.py
@@ -116,7 +116,7 @@ class Entangle(nn.Module):
 
         # Apply knowledge to the superposition of the subsignals if requested
         if self.knowledgeMask is not None:
-          superposition = superposition * isoftmax(self.knowledgeMask[jdx], dim=-2)
+          superposition = superposition * nsoftmax(self.knowledgeMask[jdx], dims=[-1,-2])
 
         # Save superposition for output if needed
         if (int(self.outputMode) & int(EntangleOutputMode.SUPERPOSITION)) != 0:

knowledge/router.py
@@ -47,12 +47,12 @@ class KnowledgeRouter(KnowledgeFilter):
 
     def forward(self, a:t.Tensor, b:t.Tensor) -> t.Tensor:
         # Find the basis vectors of the input signals
-        samples:int = max(a.size(-1), b.size(-1))
+        samples:int = self.entangleMask.size(-1)
         afft:t.Tensor = tfft.fft(a, n=samples, dim=-1)
         bfft:t.Tensor = tfft.fft(b, n=samples, dim=-1)
 
-        # Entangle the signals with the `isoftmax` function and a matmul, then sum out orthogonally
-        superposition:t.Tensor = (afft.unsqueeze(-1) @ bfft.unsqueeze(-1).transpose(-1,-2)) * isoftmax(self.entangleMask, dim=-2)
+        # Entangle the signals with the `nsoftmax` function and a matmul, then sum out orthogonally
+        superposition:t.Tensor = (afft.unsqueeze(-1) @ bfft.unsqueeze(-1).transpose(-1,-2)) * nsoftmax(self.entangleMask, dims=[-1,-2])
         afft = superposition.sum(dim=-1)
         bfft = superposition.sum(dim=-2)
 