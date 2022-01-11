619e88de91eab9d44b949e52f5c9ba4c40e9ea81
labrat97 <james@nineseven.net>
2022-01-10 16:48:18 -0800

    Fixed internal logic, now 1D router

--- knowledge/routing.py
+++ knowledge/routing.py
@@ -121,21 +121,22 @@ class KnowledgeRouter(KnowledgeFilter):
 
     def forward(self, a:t.Tensor, b:t.Tensor) -> t.Tensor:
         # Find the basis vectors of the input signals
-        samples:int = self.correlationMask.size(-1)
-        afft:t.Tensor = tfft.fft(a, n=samples, dim=-1)
-        bfft:t.Tensor = tfft.fft(b, n=samples, dim=-1)
+        afft:t.Tensor = tfft.fft(a, n=self.corrSamples, dim=-1)
+        bfft:t.Tensor = tfft.fft(b, n=self.corrSamples, dim=-1)
 
         # Entangle the signals with the `nsoftmax` function and a matmul, then sum out orthogonally
         superposition:t.Tensor = (afft.unsqueeze(-1) @ bfft.unsqueeze(-1).transpose(-1,-2)) * nsoftmax(self.correlationMask, dims=[-1,-2])
         afft = superposition.sum(dim=-1)
         bfft = superposition.sum(dim=-2)
 
-        # Flatten the batches to be able to easily index the contained filters
-        # Need to flatten at -3 due to inclusive `end_dim` argument
-        afftflat = t.flatten(afft, start_dim=0, end_dim=-3)
-        aflat = t.flatten(a, start_dim=0, end_dim=-3)
-        bfftflat = t.flatten(bfft, start_dim=0, end_dim=-3)
-        bflat = t.flatten(b, start_dim=0, end_dim=-3)
+        # Flatten the batches to be able to easily index the contained filters.
+        # In this router, the signals are being ignored in their individual channels
+        #   and flattened into the rest of the batches. This is done to maximize
+        #   the reusability of the knowledge contained.
+        afftflat = t.flatten(afft, start_dim=0, end_dim=-2)
+        aflat = t.flatten(a, start_dim=0, end_dim=-2)
+        bfftflat = t.flatten(bfft, start_dim=0, end_dim=-2)
+        bflat = t.flatten(b, start_dim=0, end_dim=-2)
         
         # Create the storage for the correlations
         icorrs:t.Tensor = t.zeros((len(self.subfilters), aflat[...,0].numel()), dtype=aflat.dtype)
@@ -161,4 +162,4 @@ class KnowledgeRouter(KnowledgeFilter):
             result[sdx].div_(topcorrs[sdx].numel())
 
         # Unflatten the resultant signals back to the relevant size
-        return nn.Unflatten(0, aflat.size()[:-2])(result)
+        return nn.Unflatten(0, aflat.size()[:-1])(result)
