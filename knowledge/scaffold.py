740f8a5f248d6e53b98946eff53ae30182462737
labrat97 <james@nineseven.net>
2022-02-16 22:42:16 -0800

    Changed to new methods

--- knowledge/scaffold.py
+++ knowledge/scaffold.py
@@ -5,21 +5,20 @@ from ..conversions import *
 from ..math import *
 from ..losses import *
 
+from .ipfs import IPFile
+
 import torch as t
 import torch.nn as nn
 
 import cid as CID
-import subprocess as proc
 
-import os
 from os import path
 
 
 class ScaffoldFilter(KnowledgeFilter):
     def __init__(self, multihash:str, ipns:bool, corrSamples:int=DEFAULT_FFT_SAMPLES, 
         inputSamples:int=DEFAULT_FFT_SAMPLES, outputSamples:int=DEFAULT_FFT_SAMPLES, 
-        cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE, ipfsMount:str=path.sep+'ipfs', 
-        ipnsMount:str=path.sep+'ipns', fastdev:str=None,
+        cdtype:t.dtype=DEFAULT_COMPLEX_DTYPE, ipfsCommand:str='ipfs', fastdev:str=None,
         weightName:str='weight', biasName:str='bias', invbiasName:str='invbias'):
 
         # Build the superclass
@@ -29,20 +28,10 @@ class ScaffoldFilter(KnowledgeFilter):
         # Iterate through the CID from IPFS, turning it into a byte array
         #   then a nn.Parameter for the module (so that it may be serialized).
         assert CID.is_cid(multihash)
-        mcid = CID.from_string(multihash)
-        mhashBytes = mcid.multihash
-        self.multihash:nn.Parameter = nn.Parameter(t.zeros((len(mhashBytes)), dtype=t.uint8, device='cpu'), requires_grad=False)
-        for idx in range(len(mhashBytes)):
-            self.multihash[idx].add_(mhashBytes[idx])
-        
-        # Read the requested file from the ipfs file system
-        if ipns:
-            self.mount:nn.Parameter = nn.Parameter(t.Tensor(path.join(ipnsMount, multihash).encode()).type(t.uint8))
-        else:
-            self.mount:nn.Parameter = nn.Parameter(t.Tensor(path.join(ipfsMount, multihash).encode()).type(t.uint8))
+        self.multihash:nn.Parameter = nn.Parameter(strToTensor(multihash), requires_grad=False)
 
         # Open the file into the CPU so that it may be potentially swapped
-        with open(bytes(self.mount).decode(), 'br') as heartfile:
+        with IPFile(multihash, ipns=ipns, command=ipfsCommand) as heartfile:
             self.heart:nn.Module = t.jit.load(heartfile, map_location='cpu')
         
         # Set the device to be used later for fast computation
@@ -51,29 +40,28 @@ class ScaffoldFilter(KnowledgeFilter):
                 fastdev = 'cuda'
             else:
                 fastdev = 'cpu'
-        self.fastdev:nn.Parameter = nn.Parameter(t.Tensor(fastdev.encode()).type(t.uint8))
+        self.fastdev:nn.Parameter = nn.Parameter(strToTensor(fastdev), requires_grad=False)
 
         # Set up the configuration flags for the model inference
-        self.freezeHeart:bool = not ipns
-        self.lstmType:bool = isinstance(self.heart, nn.LSTM)
-        self.linearType:bool = isinstance(self.heart, nn.Linear)
-        self.weightName:str = weightName
-        self.biasName:str = biasName
-        self.invbiasName:str = invbiasName
-        self.invertedLinear:bool = self.linearType
+        self.freezeHeart:nn.Parameter = nn.Parameter(t.tensor([not ipns], device='cpu'), requires_grad=False)
+        self.lstmType:nn.Parameter = nn.Parameter(t.tensor([isinstance(self.heart, nn.LSTM)], device='cpu'), requires_grad=False)
+        self.linearType:nn.Parameter = nn.Parameter(t.tensor([isinstance(self.heart, nn.Linear)], device='cpu'), requires_grad=False)
+        self.weightName:nn.Parameter = nn.Parameter(strToTensor(weightName), requires_grad=False)
+        self.biasName:nn.Parameter = nn.Parameter(strToTensor(biasName), requires_grad=False)
+        self.invbiasName:nn.Parameter = nn.Parameter(strToTensor(invbiasName), requires_grad=False)
+        self.invertedLinear:nn.Parameter = nn.Parameter(t.tensor([self.linearType], device='cpu'), requires_grad=False)
 
         # If this is a linear module, check for an inverted bias
-        if self.linearType:
-            self.invertedLinear = False
+        if self.linearType[0]:
+            self.invertedLinear[0] = False
             for name, _ in self.named_parameters(recurse=True):
                 if name == self.invbiasName:
-                    self.invertedLinear = True
+                    self.invertedLinear[0] = True
                     break
-        
     
     def cid(self):
         # Encode the contained parameter into something understandable by IPFS
-        bytelist = self.cid.tolist()
+        bytelist = self.multihash.tolist()
         return CID.from_bytes(bytearray(bytelist))
     
     def multihash(self):
